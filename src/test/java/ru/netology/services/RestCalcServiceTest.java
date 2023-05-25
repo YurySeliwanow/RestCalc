3a60147b69eb90db9092ed1999cc15982b31256c
neto test <seliwanow08@gmail.com>
2023-05-25 09:08:17 +0300

    first commit

--- src/test/java/ru/netology/services/RestCalcServiceTest.java
+++ src/test/java/ru/netology/services/RestCalcServiceTest.java
@@ -7,18 +7,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RestCalcServiceTest {
     @Test
-    public void testCalculateRest1 () {
+    public void testCalculateRest1() {
         RestCalcService service = new RestCalcService();
-        int actual = service.calculate(10_000,3_000,20_000);
-        int expected =  3;
+        int actual = service.calculate(10_000, 3_000, 20_000);
+        int expected = 3;
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void testCalculateRest2 () {
+    public void testCalculateRest2() {
         RestCalcService service = new RestCalcService();
-        int actual = service.calculate(100_000,60_000,150_000);
-        int expected =  2;
+        int actual = service.calculate(100_000, 60_000, 150_000);
+        int expected = 2;
         Assertions.assertEquals(expected, actual);
     }
 }
